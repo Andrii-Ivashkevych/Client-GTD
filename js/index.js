1a6d4bd7b0763e7a901f01356af65a8c91265175
Andrij <Andrii_Ivashkevych@epam.com>
2014-10-18 02:44:06 +0300

    Joined View.
    I joined View for migration to pages.

--- js/index.js
+++ js/index.js
@@ -25,3 +25,18 @@ var Controller = Backbone.Router.extend({
 var controller = new Controller();  //We are creating controller.
 
 Backbone.history.start();   //We are starting HTML5 History push.
+
+var Start = Backbone.View.extend({
+    el: $("#start"),    // DOM element.
+    events: {
+        "click input:button": "check"   //Event hundler for button "Check".
+    },
+    check: function(){
+        if (this.el.find("input:text").val() == "test") //Check text
+            controller.navigate("success", true);   //Migration to page success.
+        else
+            controller.navigate("error", true);  //Migration to page error.
+    }
+});
+
+var start = new Start();
